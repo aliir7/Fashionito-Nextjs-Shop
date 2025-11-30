5ecc6198bbcfa36c95e5bb2d358ee1cc722da642
ALI <alirezaei.eng7@gmail.com>
2025-11-30 20:51:05 +0330

    add Trending Section

--- Components/UI/Index/Index.tsx
+++ Components/UI/Index/Index.tsx
@@ -1,11 +1,13 @@
 import Category from "./Category/Category";
 import Hero from "./Hero/Hero";
+import Trending from "./Trending/Trending";
 
 function Index() {
   return (
     <>
       <Hero />
       <Category />
+      <Trending />
     </>
   );
 }
---
+++ Components/UI/Index/Trending/Trending.tsx
@@ -0,0 +1,19 @@
+function Trending() {
+  return (
+    <section>
+      <div className="wrapper py-20">
+        <div className="flex flex-col md:flex-row justify-between gap-5">
+          <div className="">
+            <h3 className="text-3xl font-medium">What's Trending now</h3>
+            <p className="text-lg mt-2">
+              Discover the most trending products in Fashion
+              <span className="text-secondary">ito</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Trending;
